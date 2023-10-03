7f65b1fb6065d728a381fac3acff85453878249c
LAPTOP-KI38GF6D\User <yu51200@naver.com>
2023-10-03 15:25:22 +0900

    grade_calculator_10_3_15:25

--- homework/src/grade_calculator/MajorComputer.java
+++ homework/src/grade_calculator/MajorComputer.java
@@ -1,15 +1,16 @@
 package grade_calculator;
 
-public class MajorComputer extends Major {
+public class MajorComputer {
 
 	// 필
-	Course course = new Course();
-	
-	String majorCourse = course.math;
+	Course course = new Course();	
+	String majorName;
+	String majorCourse;
 	
 	// 생
 	public MajorComputer() {
-		
+		majorName = "컴퓨터공학과";
+		majorCourse = course.math;
 	}
 	//메
 	
--- homework/src/grade_calculator/MajorKorean.java
+++ homework/src/grade_calculator/MajorKorean.java
@@ -1,15 +1,16 @@
 package grade_calculator;
 
-public class MajorKorean extends Major {
+public class MajorKorean { 
 
 	// 필
-	Course course = new Course();
-	
-	String majorCourse = course.korean;
+	Course course = new Course();	
+	String majorName;
+	String majorCourse;
 	
 	// 생
 	public MajorKorean() {
-		
+		majorName = "국어국문학과";
+		majorCourse = course.korean;
 	}
 	//메
 	
